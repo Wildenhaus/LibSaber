fbc100533128e28fd25ac0e3e2da9f4b7af4265d
Haus <reid@wilden.haus>
2022-11-26 18:02:55 -0500

    Add project structure

---
+++ src/LibSaber.HaloCEA/Usings.cs
@@ -0,0 +1,8 @@
+﻿global using static LibSaber.Assertions;
+
+#region Default Types
+
+global using SentinelId = System.Int16;
+global using Sentinel = LibSaber.Shared.Sentinel<System.Int16, System.Int32>;
+
+#endregion
---
+++ src/LibSaber/Shared/Sentinel.cs
@@ -0,0 +1,38 @@
+﻿using LibSaber.IO;
+
+namespace LibSaber.Shared
+{
+
+  public readonly struct Sentinel<TSentinel, TOffset>
+    where TSentinel : unmanaged
+    where TOffset : unmanaged
+  {
+
+    #region Data Members
+
+    public readonly TSentinel Id;
+    public readonly TOffset EndOffset;
+
+    #endregion
+
+    #region Constructor
+
+    public Sentinel( TSentinel id, TOffset endOffset )
+    {
+      Id = id;
+      EndOffset = endOffset;
+    }
+
+    public static Sentinel<TSentinel, TOffset> Read( NativeReader reader )
+    {
+      var id = reader.ReadUnmanaged<TSentinel>();
+      var endOffset = reader.ReadUnmanaged<TOffset>();
+
+      return new Sentinel<TSentinel, TOffset>( id, endOffset );
+    }
+
+    #endregion
+
+  }
+
+}
