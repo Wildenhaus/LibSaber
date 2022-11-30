6f3b4d8efb28434c65fe80e821dd008c2cdf2ae3
Haus <reid@wilden.haus>
2022-11-30 18:51:51 -0500

    Add an SNorm8 conversion method to NumericExtensions

--- src/LibSaber/Extensions/NumericExtensions.cs
+++ src/LibSaber/Extensions/NumericExtensions.cs
@@ -6,6 +6,9 @@ namespace LibSaber.Extensions
   public static class NumericExtensions
   {
 
+    public static float SNormToFloat( this sbyte snormValue )
+      => snormValue / ( float ) sbyte.MaxValue; // TODO
+
     public static float SNormToFloat( this short snormValue )
       => snormValue / SNorm16.Coefficient;
 
